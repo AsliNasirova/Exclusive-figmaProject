3d2c53c6668f9db51e52544d2237d272e2634306
AsliNasirova <tu7MXGB4G@code.edu.az>
2023-12-19 12:26:31 -0800

    added

--- Exclusive/src/Pages/HomePage/home.jsx
+++ Exclusive/src/Pages/HomePage/home.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import HeaderPageComponents from '../../Components/HeaderPageComponents/headerPageComponents'
+import FlashSalesSection from '../../Components/FlashSalesSection'
+import Category from '../../Components/BrowseByCategory/category'
+import Music from '../../Components/MusicExperience/music'
+import Arrival from '../../Components/NewArrival/arrival'
+import FastAndFree from '../../Components/FreeAndFastCustomerSection/fast'
 
 function Home() {
   return (
     <div>
      <HeaderPageComponents/>
+     <FlashSalesSection/>
+     <Category/>
+     <Music/>
+     <Arrival/>
+     <FastAndFree/>
+    
     </div>
   )
 }
